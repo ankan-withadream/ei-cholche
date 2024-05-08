1589b7a221c027187f07974d5fc8d202922b84e0
ama3it <adhikariamit2001@gmail.com>
2024-05-08 22:22:09 +0530

    Routes 🚀 01. Added react-router-dom dependencie for react routing 02. Added /contacts, /profiles, /chats Route to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./App.css";
 import Layout from "./layout";
 import Chats from "./pages/Chats";
 import Contacts from "./pages/Contacts";
 import Profile from "./pages/Profile";
 
+
 function App() {
   return (
-    <Layout>
-      <Contacts />
-    </Layout>
+    <Router>
+      <Routes>
+        <Route
+          path="/chats"
+          element={
+            <Layout>
+              <Chats />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/contacts"
+          element={
+            <Layout>
+              <Contacts />
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <Profile />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
