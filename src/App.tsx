5b6cb9bf05d2082ca97f511ede7125b3da906e21
ama3it <adhikariamit2001@gmail.com>
2024-05-08 22:15:49 +0530

    Profile Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import "./App.css";
 
 import Layout from "./layout";
 import Chats from "./pages/Chats";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Layout>
-      <Chats />
+      <Profile />
     </Layout>
   );
 }
