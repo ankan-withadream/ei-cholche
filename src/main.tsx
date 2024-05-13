d7bf30e11eafd82d06818ca88a769a7d9a65221b
Ankan <ankanwithadream@gmail.com>
2024-05-13 14:32:59 +0000

    Auth0 login implemented

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <div className="lg:flex">
-      <App />
-    </div>
-  </React.StrictMode>
+  <Auth0Provider
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      redirect_uri: import.meta.env.VITE_AUTH0_REDIRECT_URL,
+    }}
+  >
+    <React.StrictMode>
+      <div className="lg:flex">
+        <App />
+      </div>
+    </React.StrictMode>
+  </Auth0Provider>
 );
